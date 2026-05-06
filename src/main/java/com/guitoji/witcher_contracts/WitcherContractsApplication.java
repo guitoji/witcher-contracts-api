1fe9ebf805d12a1d6ee7c8ea19deba138ff4c8fe
guitoji <guilherme.limaal22@outlook.com>
2026-05-06 16:04:00 -0300

    project created and postgres database configured

---
+++ src/main/java/com/guitoji/witcher_contracts/WitcherContractsApplication.java
@@ -0,0 +1,13 @@
+package com.guitoji.witcher_contracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitcherContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitcherContractsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guitoji/witcher_contracts/WitcherContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guitoji.witcher_contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitcherContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
