3e37d29b423c9f2aa31ba1250d04a2053c6fa808
guitoji <guilherme.limaal22@outlook.com>
2026-05-13 00:31:57 -0300

    Create endpoint created to Kingdom

--- src/main/java/com/guitoji/witcher_contracts/controller/KingdomController.java
+++ src/main/java/com/guitoji/witcher_contracts/controller/KingdomController.java
@@ -1,9 +1,15 @@
 package com.guitoji.witcher_contracts.controller;
 
+import com.guitoji.witcher_contracts.dto.request.KingdomDTO;
 import com.guitoji.witcher_contracts.service.KingdomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/kingdoms")
@@ -12,5 +18,9 @@ public class KingdomController implements GenericController {
 
     private final KingdomService kingdomService;
 
-
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody KingdomDTO dto) {
+        URI locate = getHeaderLocation(kingdomService.save(dto).getId());
+        return ResponseEntity.created(locate).build();
+    }
 }
--- src/main/java/com/guitoji/witcher_contracts/service/KingdomService.java
+++ src/main/java/com/guitoji/witcher_contracts/service/KingdomService.java
@@ -1,12 +1,23 @@
 package com.guitoji.witcher_contracts.service;
 
+import com.guitoji.witcher_contracts.dto.request.KingdomDTO;
+import com.guitoji.witcher_contracts.mapper.KingdomMapper;
+import com.guitoji.witcher_contracts.model.Kingdom;
 import com.guitoji.witcher_contracts.repository.KingdomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class KingdomService {
 
     private final KingdomRepository kingdomRepository;
+    private final KingdomMapper kingdomMapper;
+
+    @Transactional
+    public Kingdom save(KingdomDTO dto) {
+        Kingdom kingdom = kingdomMapper.toEntity(dto);
+        return kingdomRepository.save(kingdom);
+    }
 }
