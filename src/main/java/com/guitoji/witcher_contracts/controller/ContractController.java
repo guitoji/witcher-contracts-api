b6898bf6f06684923d14eb5dfaf65eaf1e86cf00
guitoji <guilherme.limaal22@outlook.com>
2026-05-16 03:08:50 -0300

    Create endpoint was created to Contract

--- src/main/java/com/guitoji/witcher_contracts/controller/ContractController.java
+++ src/main/java/com/guitoji/witcher_contracts/controller/ContractController.java
@@ -1,16 +1,27 @@
 package com.guitoji.witcher_contracts.controller;
 
+import com.guitoji.witcher_contracts.dto.request.ContractDTO;
 import com.guitoji.witcher_contracts.service.ContractService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/contracts")
 @RequiredArgsConstructor
 public class ContractController implements GenericController {
 
-    private ContractService contractService;
+    private final ContractService contractService;
 
-
+    @PostMapping
+    public ResponseEntity<Object> create(@Valid @RequestBody ContractDTO dto) {
+        URI locate = getHeaderLocation(contractService.save(dto).getId());
+        return ResponseEntity.created(locate).build();
+    }
 }
--- src/main/java/com/guitoji/witcher_contracts/service/ContractService.java
+++ src/main/java/com/guitoji/witcher_contracts/service/ContractService.java
@@ -1,5 +1,8 @@
 package com.guitoji.witcher_contracts.service;
 
+import com.guitoji.witcher_contracts.dto.request.ContractDTO;
+import com.guitoji.witcher_contracts.mapper.ContractMapper;
+import com.guitoji.witcher_contracts.model.Contract;
 import com.guitoji.witcher_contracts.repository.ContractRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,10 @@ import org.springframework.stereotype.Service;
 public class ContractService {
 
     private final ContractRepository contractRepository;
+    private final ContractMapper contractMapper;
+
+    public Contract save(ContractDTO dto) {
+        Contract contract = contractMapper.toEntity(dto);
+        return contractRepository.save(contract);
+    }
 }
